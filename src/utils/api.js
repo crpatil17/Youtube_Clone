4f842bc0224ec5529986085c200e3353c6d6e571
crpatil17 <chetanrpatil17@gmail.com>
2024-04-27 11:17:02 +0530

    youtube clone created successfully

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key":
+      process.env.REACT_APP_YOUTUBE_API_KEY ||
+      "5d0c8883a0msh7fae1f3f5639e4bp13ecfdjsne6e3911b4244",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
